5148fedc45461ab44e033c65efc3536f45256837
histonemark <corralesmarc@gmail.com>
2021-01-13 17:14:32 +0000

    Added graphing and fixed classes. Lacks animation for auto-updating the plot

--- fibrem.py
+++ fibrem.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TKAgg")
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
 import numpy as np
 import os
 import pdb
 import time
 from skimage.filters import gaussian
 from skimage import io
-from tkinter import *
+import tkinter as tk 
 from tkinter import filedialog
 from tkinter.messagebox import showinfo
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
-
-root = Tk()
-root.title('FibRem: Seeping aid for EM microscopists')
-
-
+        
 class Watchdog(PatternMatchingEventHandler, Observer):
-    def __init__(self, path, patterns, logfunc):
+    def __init__(self, path='.', patterns="*" ,*args, **kwargs):
         PatternMatchingEventHandler.__init__(self, patterns)
         Observer.__init__(self)
         self.schedule(self, path=path, recursive=False)
@@ -39,39 +39,41 @@ class Watchdog(PatternMatchingEventHandler, Observer):
         # This function is called when a file is moved    
         self.log(f"Someone moved {event.src_path} to {event.dest_path}")
 
-class LeftFrame:
-    def __init__(self, master):
-        frm = Frame(master)
         
-        # Watchdog init
+class LeftFrame(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, bg="red" ,*args, **kwargs)
+        self.parent = parent
+
+        # Watchdog status
         self.watchdog = None
         self.watch_path = None
     
         # People init Off-duty 
-        self.ana_duty = IntVar(0)
-        self.eliz_duty = IntVar(0)
-        self.marc_duty = IntVar(0)
+        self.ana_duty = tk.IntVar(0)
+        self.eliz_duty = tk.IntVar(0)
+        self.marc_duty = tk.IntVar(0)
 
         # Generate checkbuttons
-        Label(master, text='Select people to notify: ').pack(pady=5,  anchor=W)
-        Checkbutton(master, text='Ana', variable=self.ana_duty).pack(pady=5, anchor=W)
-        Checkbutton(master, text='Elisabeth', variable=self.eliz_duty).pack(pady=5, anchor=W)
-        Checkbutton(master, text='Marc', variable=self.marc_duty).pack(pady=5, anchor=W)
+        self.lbl1 = tk.Label(self, text='Select people to notify: ').pack(pady=5,  anchor=tk.W)
+        self.cbtn1 = tk.Checkbutton(self, text='Ana', variable=self.ana_duty).pack(pady=5, anchor=tk.W)
+        self.cbtn2 = tk.Checkbutton(self, text='Elisabeth', variable=self.eliz_duty).pack(pady=5, anchor=tk.W)
+        self.cbtn3 = tk.Checkbutton(self, text='Marc', variable=self.marc_duty).pack(pady=5, anchor=tk.W)
 
         # Generate monitoring buttons
-        Label(master, text=' ').pack(pady=5)
-        Button(master, text='Select Folder', command=self.select_path).pack(anchor=S+W)
-        Button(master, text='Start Monitoring', command=self.start_watchdog).pack(anchor=S+W)
-        Button(master, text='Stop Monitoring', command=self.stop_watchdog).pack(anchor=S+W)
-        Button(master, text='Exit', command=root.destroy).pack(anchor=S+W)
-        frm.pack(fill=Y, expand=1)
+        self.lbl2 = tk.Label(self, text=' ').pack(pady=5)
+        self.btn4 = tk.Button(self, text='Select Folder', command=self.select_path).pack(anchor=tk.S+tk.W)
+        self.btn5 = tk.Button(self, text='Start Monitoring', command=self.start_watchdog).pack(anchor=tk.S+tk.W)
+        self.btn6 = tk.Button(self, text='Stop Monitoring', command=self.stop_watchdog).pack(anchor=tk.S+tk.W)
+        self.btn7 = tk.Button(self, text='Exit', command=root.destroy).pack(anchor=tk.S+tk.W)
+    
         
     def start_watchdog(self):
         if not self.watch_path:
             self.select_path()
             
         if self.watchdog is None:
-            self.watchdog = Watchdog(path=self.watch_path, patterns="*.tiff", logfunc=self.log)
+            self.watchdog = Watchdog(path=self.watch_path, patterns="*.tiff")
             self.watchdog.start()
             self.log('Watchdog started')
         else:
@@ -90,10 +92,7 @@ class LeftFrame:
         path = filedialog.askdirectory()
         if path:
             self.watch_path = path
-            self.log(f'Selected path: {path}')
-        
-    def log(self, message):
-        showinfo(root, message=f'{message}\n')
+            self.log(f'Selected path: {path}')    
                  
         
 def get_focus_index(imgpath):
@@ -114,17 +113,37 @@ def get_focus_index(imgpath):
     f.write(imgname + "\t" + str(focus_index) + "\n")
     f.close()
     
-    
+        
+class PlotsFrame(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.lbl1 = tk.Label(self, text='Vital constants plots').pack(pady=5)
 
-    
-class PlotsFrame:
-    def __init__(self, master):
-        frm = Frame(master)
-        frm.pack()
-        self.focus_index = np.random.normal(0.34, 0.1, 3000)
-        plt.hist(focus_index)
-        plt.show()
-    
+        fig = Figure(figsize=(10, 8), dpi=100)
+        t = np.arange(0, 3, .01)
+        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 
-lf = LeftFrame(root)
-root.mainloop()
+        canvas = FigureCanvasTkAgg(fig, master=self)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas.get_tk_widget().pack()
+
+        
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.left_frame = LeftFrame(self)
+        self.plots_frame = PlotsFrame(self)
+        
+        self.left_frame.pack(side="left", fill="y")
+        self.plots_frame.pack(side="right", fill="both", expand=True)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('FibRem: Seeping aid for EM microscopists')
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
